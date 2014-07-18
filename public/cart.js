ce68b9977c0a2a1be133b5f85bfb2451ece43d19
Ciaran Hale <ciaran.hale@optus.com.au>
2014-07-18 14:15:14 +1000

    Added ajax and larger data files

--- public/cart.js
+++ public/cart.js
@@ -1,40 +1,41 @@
-var cartData = new Blaze.Var([
-	{
-		quantity: "2",
-		monthlyCost: "20",
-		productTitle: "Phone",
-		upfrontCost: "40"
-	},
-	{
-		quantity: "1",
-		monthlyCost: "10",
-		productTitle: "Modem",
-		upfrontCost: "100"
-	}
-]);
+var cartData = new Blaze.Var();
+$.ajax({
+       url: "testingJson.json",
+           //force to handle it as text
+       dataType: "text",
+            success: function (dataTest) {
 
+                //data downloaded so we call parseJSON function
+                //and pass downloaded data
+                var json = $.parseJSON(dataTest);
+                cartData.set(json);
+
+
+
+             }
+ });
 Template.main.events({
 	'click .update': function () {
-		cartData.set([
-			{
-				quantity: "3",
-				monthlyCost: "10",
-				productTitle: "Shoes",
-				upfrontCost: "40"
-			},
-			{
-				quantity: "1",
-				monthlyCost: "10",
-				productTitle: "Bags",
-				upfrontCost: "100"
-			},
-		]);
+		$.ajax({
+			url: "updatedJson.json",
+			//force to handle it as text
+				dataType: "text",
+					success: function (dataTest) {
+
+					//data downloaded so we call parseJSON function
+					//and pass downloaded data
+					var json = $.parseJSON(dataTest);
+					cartData.set(json);
+
+
+	             }
+	  });
 	}
 });
-
 Template.main.helpers({
 	product: function () {
 		return cartData.get();
 	},
 
 });
+
